b8b6dbb9ae114ff14c066696a4a0699a3f6a74ce
Oleksandr Mykhailiuta <mykhailiuta.aleksandr@corp.mailfire.io>
2018-10-24 19:03:54 +0300

    tmp

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -2,42 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	printFormattedLine(0,"zero")
-	printFormattedLine(1,"one_1")
-	printFormattedLine(1,"one_2")
-	printFormattedLine(2,"two")
-
-	//out := os.Stdout
-	//if !(len(os.Args) == 2 || len(os.Args) == 3) {
-	//	panic("usage go run main.go . [-f]")
-	//}
-	//path := os.Args[1]
-	//printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	//err := dirTree(out, path, printFiles)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
-//func dirTree(out io.Writer, path string, printFiles bool) error {
-//	fullPath, _ := filepath.Abs(path)
-//	var files []string
-//
-//	err := filepath.Walk(fullPath, func(path string, info os.FileInfo, err error) error {
-//		files = append(files, path)
-//		return nil
-//	})
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	return nil
-//}
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	fullPath, _ := filepath.Abs(path)
+	//var files []string
 
-func printFormattedLine(position int, line string){
+	err := filepath.Walk(fullPath, func(path string, info os.FileInfo, err error) error {
+		formattedName := info.Name()
+		if !info.IsDir() {
+			formattedName += " ("
+			if info.Size() == 0 {
+				formattedName += "empty)"
+			} else {
+				formattedName += strconv.FormatInt(info.Size(), 10)+"b)"
+			}
+		}
+		relativePath := strings.Replace(path, fullPath+"/", "", -1)
+		if fullPath == relativePath{
+			return nil
+		}
+		position := strings.Count(relativePath, "/")
+		printFormattedLine(position, formattedName)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func printFormattedLine(position int, line string, lastInGroup bool){
 	var outputLine string
 
 	if (position > 0){
